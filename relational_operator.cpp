e73995cb521108b13ad2110962091cd79e81283b
pratham0709 <109234076+pratham0709@users.noreply.github.com>
2022-08-03 20:50:52 +0530

    Create relational_operator.cpp

---
+++ relational_operator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std; 
+int main(){
+	int a = 5;
+	int b = 9;
+	
+	bool first = (a==b);
+	cout<<first<<endl;
+	
+	bool second = (a > b);
+	cout<<second<<endl;
+	
+	bool third = a < b;
+	cout<< third <<endl;
+	
+	bool fourth = a>= b;
+	cout<<fourth<<endl;
+	
+	bool fifth = a<=b;
+	cout<<fifth<<endl;
+	
+	bool sixth = a!= b;
+	cout<<sixth<<endl;
+	
+}
