92f37e8ebf8dbd607234a7a028d866fd500dc78a
pratham0709 <109234076+pratham0709@users.noreply.github.com>
2022-08-04 05:56:46 +0530

    Update condition_01.cpp

--- condition_01.cpp
+++ condition_01.cpp
@@ -14,3 +14,13 @@ int main(){
 	}
 	return 0;
 }
+
+
+// OUTPUT.........
+Enter The Value Of a :
+5
+a is Positive
+
+--------------------------------
+Process exited after 2.933 seconds with return value 0
+Press any key to continue . . .
