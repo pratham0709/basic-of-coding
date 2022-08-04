06decf4e500ff1fd891f5e90315f330b34c75250
pratham0709 <109234076+pratham0709@users.noreply.github.com>
2022-08-04 06:08:59 +0530

    Create condition_02.cpp

---
+++ condition_02.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a, b;
+	cout<<"Enter The Value Of a:"<<endl;
+	cin>>a;
+	
+	cout<<"Enter The Value Of b "<<endl;
+	cin>>b;
+	
+	if(a>b){
+		cout<<" The Value Of A is Greater Than B "<<endl;
+	}
+	else{
+		cout<<"The Value Of B is Greater Than A "<<endl;
+	}
+}
+
+/* OUTPUT........
+Enter The Value Of a:
+2
+Enter The Value Of b
+6
+The Value Of B is Greater Than A
+
+--------------------------------
+Process exited after 3.52 seconds with return value 0
+Press any key to continue . . .
+*/
+  
